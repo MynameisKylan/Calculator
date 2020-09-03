384191c7a0487ac43054670f23652293462028b5
Kylan Wong <mynameiskylan@gmail.com>
2020-09-03 16:41:45 -0700

    Added numpad support, changed handling of divide by zero/decimal

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Core operation functions
 function add(a, b) {
     return a + b;
 }
@@ -18,6 +19,7 @@ function operate(op, a, b) {
     return op(a, b);
 }
 
+// Helper Functions for updateDisplay()
 function clear() {
     op = '';
     a = '';
@@ -31,7 +33,7 @@ function updateHistory(op, a, b) {
     child.textContent = expression;
     history.appendChild(child);
     if (history.childElementCount > 10) {
-        console.log(history.childNodes)
+        // console.log(history.childNodes)
         history.removeChild(history.childNodes[3]);
     }
 }
@@ -49,6 +51,15 @@ function deleteLast() {
     }
 }
 
+function isNumberKey(keyCode) {
+    return ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 105))
+}
+
+function isOperatorKey(keyCode) {
+
+}
+
+// Primary logic
 function updateDisplay(display, text) {
     if (text === 'Clear') {
         clear();
@@ -57,19 +68,22 @@ function updateDisplay(display, text) {
     } else if (a !== '' && op && b !== '' && (text === '=' || text in operators)){
         // console.log('pop!')
         let eval = operate(operators[op], parseFloat(a), parseFloat(b)).toString();
-        updateHistory(op, a, b);
-        clear();
         if (eval === 'NaN') {
-            alert('Can\'t divide by zero!');
+            alert('Error');
             clear();
         } else {
             displayValue = eval;
+            updateHistory(op, a, b);
+            clear();
             a = eval;
+            displayValue = a;
         }
         if (text in operators) {
             op = text
         }
-    } else if (text === '=' || (text === '.' && displayValue.includes('.'))) {
+    } else if (text === '=') {
+        op = '';
+    } else if (text === '.' && displayValue.includes('.')) {
         // do nothing
     } else if (op && text in operators){
         op = text;
@@ -94,16 +108,30 @@ function updateDisplay(display, text) {
     display.textContent = (displayValue) ? displayValue : '0';
 }
 
+// Initialization
 const display = document.querySelector('#display');
 const buttons = Array.from(document.querySelectorAll('button'));
 const operators = {'÷': divide, '×': multiply, '−': subtract, '+': add};
+const numPadOperators = {'Enter':'=', 'Add':'+', 'Subtract':'−', 'Multiply':'×', 'Divide':'÷', 'Decimal':'.'};
 const history = document.querySelector('#history');
 let displayValue = '0';
 let op = '';
 let a = '';
 let b = '';
+
+// Event listeners for buttons
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         updateDisplay(display, button.textContent)
     });
 })
+
+// Event listeners for keypresses
+window.addEventListener('keydown', (event) => {
+    let key = event.code;
+    if (key.includes('Numpad')) {
+        key = key.replace('Numpad', '');
+        console.log(key);
+        updateDisplay(display, (key in numPadOperators) ? numPadOperators[key] : key);
+    }
+});
